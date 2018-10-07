ca57cbf4abfedc1f75858f9e0a52c1c942578eb4
rrortegaa <rrortegaa@gmail.com>
2018-10-07 14:20:23 -0500

    Datos dummy de mapa, primera version

--- hackaton_teleton/public/js/map.js
+++ hackaton_teleton/public/js/map.js
@@ -1,10 +1,46 @@
+// var data = [
 
+//   { Estados: 
+//     {
+//     Aguascalientes:  {
+//       donantes: 2000,
+//       monto: 250000
+//     }},
+//     { ['Baja California']: {
+//       donantes: 2000,
+//       monto: 30000
+//     }},
+
+
+//   }
+
+// ]
+
+var data = {
+  aguascalientes : {
+    donantes : 1000,
+    monto : 20000
+  },
+  ['baja california'] : {
+    donantes : 2400,
+    monto : 26000
+  },
+  guanajuato : {
+    donantes : 2000,
+    monto : 24000
+  },
+}
+
+
+  
   $(document).ready(function(){
       // Aqui va todo mi jquery #s07 > path
       $("#states > a").on("click", function(){
         var textos = $(this).find('text')
         var estado = ''
+        console.warn('ESTADO:', estado)
         if (textos.length > 0) estado = textos[0].innerHTML
         console.warn('ESTADO: ', estado)
       })
   })
+
